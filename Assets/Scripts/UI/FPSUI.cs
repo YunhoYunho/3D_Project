901e1c9b42faac9bc117dc61b7bfb29a0a58c809
Yunho <115628325+YunhoYunho@users.noreply.github.com>
2023-01-12 21:02:48 +0900

    feat : FPS 수치 추가

---
+++ Assets/Scripts/UI/FPSUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FPSUI : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    private int frameCount = 0;
+    private float fps = 0;
+    private float timeLeft = 0.5f;
+    private float timePassed = 0f;
+    private float updateInterval = 0.5f;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        if (!text)
+        {
+            Debug.LogError("text 컴포넌트가 없습니다!");
+            enabled = false;
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        frameCount += 1;
+        timeLeft -= Time.deltaTime;
+        timePassed += Time.timeScale / Time.deltaTime;
+
+        if (timeLeft <= 0f)
+        {
+            fps = timePassed / frameCount;
+            timeLeft = updateInterval;
+            timePassed = 0f;
+            frameCount = 0;
+        }
+
+        text.text = string.Format("{0} FPS", fps);
+    }
+}

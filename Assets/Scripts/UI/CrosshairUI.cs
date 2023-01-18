2a3fa76f8ffc00de8f7bc59be91c0fa120ab0825
bigunit3030@gmail.com <bigunit3030@gmail.com>
2023-01-19 00:04:09 +0900

    fix : Crosshair 수정

--- Assets/Scripts/UI/CrosshairUI.cs
+++ Assets/Scripts/UI/CrosshairUI.cs
@@ -8,60 +8,18 @@ public class CrosshairUI : MonoBehaviour
     private Animator animator;
 
     [SerializeField]
-    private GameObject CrosshairHUD;
+    private GameObject crosshairHUD;
 
     [SerializeField]
     private Gun gun;
 
-    [SerializeField]
-    private float gunRecoil;
-
-    public void Moving(bool flag)
+    public void Jumping(bool flag)
     {
-        animator.SetBool("Moving", flag);
+        animator.SetBool("Jumping", flag);
     }
 
     public void Crouching(bool flag)
     {
         animator.SetBool("Crouching", flag);
-    }
-
-    public void Fire()
-    {
-        if (animator.GetBool("Moving"))
-        {
-            animator.SetTrigger("Move_Fire");
-        }
-
-        else if (animator.GetBool("Crouching"))
-        {
-            animator.SetTrigger("Crouch_Fire");
-        }
-
-        else
-        {
-            animator.SetTrigger("Idle_Fire");
-        }
-    }
-
-    public float GetRecoil()
-    {
-        if (animator.GetBool("Moving"))
-        {
-            gunRecoil = 0.05f;
-        }
-
-        else if (animator.GetBool("Crouching"))
-        {
-            gunRecoil = 0.02f;
-        }
-
-        else
-        {
-            gunRecoil = 0.03f;
-
-        }
-
-        return gunRecoil;
     }
 }

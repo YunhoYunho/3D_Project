3e3e58c174f0bb24e315709a0c99914b6d17c53a
Yunho <115628325+YunhoYunho@users.noreply.github.com>
2023-01-27 16:06:15 +0900

    refactor : UI, Unit 미사용 using 제거

--- Assets/Scripts/UI/FPSUI.cs
+++ Assets/Scripts/UI/FPSUI.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
 public class FPSUI : MonoBehaviour
--- Assets/Scripts/Unit/Player/Gun/GunData.cs
+++ Assets/Scripts/Unit/Player/Gun/GunData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // 초기 설정 값
